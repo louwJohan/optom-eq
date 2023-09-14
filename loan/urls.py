d5ef71a72da999b82a7609391f4cd6bba154d499
louwJohan <louwjohanc@gmail.com>
2023-09-14 13:07:48 +0100

    Seperate ref and occular health forms to 2 seperate pages

--- loan/urls.py
+++ loan/urls.py
@@ -4,4 +4,6 @@ from loan import views
 urlpatterns = [
     path('', views.loan, name='loan'),
     path('longloan', views.longloan, name='longloan'),
+    path('refraction', views.refraction, name='refraction'),
+    path('health', views.health, name='health')
 ]
--- loan/views.py
+++ loan/views.py
@@ -3,8 +3,30 @@ from loan.models import Student
 
 # Create your views here.
 def loan(request):
-    students = Student.objects.all()
-    return render(request, 'loan.html', {'students': students})
+    return render(request, 'loan.html')
 
 def longloan(request):
-    return render(request, 'longloan.html')
+    students = Student.objects.all()
+    return render(request, 'longloan.html', {'students': students})
+
+def refraction(request):
+    if request.method == 'POST':
+        student = request.POST.get('student')
+        room = request.POST.get('room')
+        ret = request.POST.get('ret')
+        refbox = request.POST.get('refbox')
+        modeleye = request.POST.get('modeleye')
+        budgy = request.POST.get('budgy')
+        pdruler = request.POST.get('pdruler')
+        occluder = request.POST.get('occluder')
+        print(student, room, ret, refbox, modeleye, budgy, pdruler, occluder)
+    students = Student.objects.all()
+    return render(request, 'refraction.html', {'students': students})
+
+def health(request):
+    if request.method == 'POST':
+        student = request.POST.get('student')
+        room = request.POST.get('room')
+        print(student, room)
+    students = Student.objects.all()
+    return render(request, 'health.html', {'students': students})
