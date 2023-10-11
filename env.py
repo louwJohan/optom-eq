a6857177b070fc3fd114f8e7b6df954281b2ebc1
louwJohan <louwjohanc@gmail.com>
2023-10-11 10:37:18 +0100

    Removed env.py

--- env.py
+++
@@ -1,6 +0,0 @@
-import os 
-
-# os.environ['DATABASE_URL'] = 'postgres://bradford_user:bzNRcp2HJGz5ppbEUVMErG5MQv8OXQUD@dpg-ckj54cglk5ic73caa7i0-a/bradford'
-os.environ['SECRET_KEY'] = 'mywagwoord@qwerty!0987654321'
-os.environ['DEV'] = '1'
-# os.environ['ALLOWED_HOSTS'] = 
